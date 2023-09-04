f8a9abdab3f18619723259156763a96eee65ed15
HHO TruongNL <truongnl@hunghau.vn>
2023-09-04 17:23:20 +0700

    update

--- 4.While.py
+++ 4.While.py
@@ -12,7 +12,7 @@ def bai34():
 # Nhập n. Cho S(k) = 1 + 2 + 3 + … + k
 # Tìm k sao cho S(k) lớn nhất nhưng nhỏ hơn n
 def bai35():
-    n = int(input("Nhập n: "))
+    n = int(input("N=: "))
     while n < 0:
         n = int(input("Nhập lại n: "))
     
@@ -33,30 +33,95 @@ def bai36():
     A = float(input("Nhập A:"))
     n = 0
     S = 0
-    while S >= A:
+    while S <= A:
         n += 1
         S += 1/n
         print("n nhỏ nhất là:", n)
 
-bai36()
+# bai36()
 
 # Bài 37
 # Nhập vào một dãy số nguyên, ngưng nhập khi người dùng nhập -1.
 # Sau khi nhập xong, in số lớn nhất, số nhỏ nhất trong những số vừa nhập
-# def bai37():
+def bai37():
+    max_num = float('-inf')
+    min_num = float('inf')
+    while True:
+        num = int(input("Nhập số nguyên (Nhập -1 để kết thúc):"))
+        if num == -1:break 
+        if num > max_num: max_num = num
+        if num < min_num: min_num = num
+    if max_num == float('-inf') or min_num == float('inf'):print("Bạn chưa nhập số nào")
+    else:print("Số lớn nhất là:", max_num,"\nSố nhỏ nhất là:",min_num)
+# bai37()
 
 # Bài 38: Nhập vào số nguyên dương n, đếm xem n có bao nhiêu chữ số
-# def bai38():
+def bai38():
+    n = int(input("N=: "))
+    dem = 0
+    while n >= 10:
+        n /= 10
+        dem +=1
+        print("số có",dem +1,"ký tự")
+# bai38()
 
 # Bài 39: Nhập vào số nguyên dương n, đếm xem n có bao nhiêu chữ số chẵn, bao nhiêu chữ số lẻ
-# def bai39():
+def bai39():
+    n = int(input("N=: "))
+    chan = 0
+    le = 0
+    while n > 0:
+        socuoi = n % 10
+        if socuoi % 2 == 0:
+            chan += 1
+        else:
+            le += 1
+        n //= 10
+    print("Số chẵn:", chan,"\nSố lẻ:", le,"\nTổng các số:",chan+le)
+# bai39()
+
 
 # Bài 40: Nhập vào số nguyên dương n, tính tổng các chữ số của n
-# def bai40():
+def bai40():
+    n = int(input("N=: "))
+    nn = n
+    tong = 0
+    while n > 0:
+        tong += n % 10
+        n //= 10
+    print("Tổng các chữ số của",nn,"=",tong)
+# bai40()
+        
 
 # Bài 41: Nhập vào một số nguyên dương n, kiểm tra xem n có phải là số dạng 2^k hay không
-# def bai41():
+def bai41():
+    n = int(input("N="))
+    a=n
+    while n % 2 == 0:
+        n //= 2
+    if n == 1:
+        print(a,"là dạng 2^k")
+    else:
+        print(a,"không phải là dạng 2^k")
+# bai41()
+        
 
 # Bài 42:Dãy số fibonacci là dãy số được định nghĩa như sau: 1, 1, 2, 3, 5, 8, 13,... với số kế tiếp sẽ bằng tổng hai số trước đó
 # Nhập vào A, hãy tìm số trong dãy số fibonacci lớn nhất nhưng không vượt quá A
-# def bai42():
+def bai42():
+    a = int(input("A="))
+    fib1 = 1
+    fib2 = 1
+    max_fib = 1
+
+    while True:
+        next_fib = fib1 + fib2
+        if next_fib <= a:
+            max_fib = next_fib
+            fib1 = fib2
+            fib2 = next_fib
+        else:
+            break
+
+    print("Số Fibonacci lớn nhất không vượt quá",a,"=", max_fib)
+bai42()
--- 5.Strings.py
+++ 5.Strings.py
@@ -1,10 +1,117 @@
-# def bai43():
-# def bai44():
-# def bai45():
-# def bai46():
+# Bài 43: Nhập vào một chuỗi, hãy đếm xem trong chuỗi có bao nhiêu từ 
+# (quy định là chuỗi không có ký tự đặc biệt, không số, không có dấu câu, chỉ có ký tự chữ và khoảng trắng)
+def bai43():
+    strings = str(input("Nhập chuỗi:"))
+    print(len(strings))
+# bai43()
+
+# Bài 44: Nhập vào một chuỗi, hãy in từ đầu tiên trong chuỗi
+def bai44():
+    strings = str(input("Nhập chuỗi:"))
+    print("Từ đầu tiên là",strings[0])
+# bai44()
+
+# Bài 45: Nhập vào một chuỗi có dạng 3 số nguyên, mỗi số nguyên cách nhau một dấu phẩy, hãy tính tổng 3 số nguyên đó
+# VD:Nhập: 3, 12, 15 Tổng: 30
+def bai45():
+    string = input("Nhập số chuỗi:")
+    numbers = string.split(",")
+    sum = 0
+    for i in numbers:
+        sum += int(i)
+    print(sum)
+# bai45()
+
+# Bài 46: Nhập vào một chuỗi, hãy đếm xem trong chuỗi có bao nhiêu ký tự in hoa, bao nhiêu ký tự in thường, bao nhiêu ký tự số
+def bai46():
+    strings = str(input("Nhập chuỗi:"))
+    inSupper = 0
+    inLower = 0
+    inDigit = 0
+    for char in strings:
+        if char.isupper(): inSupper += 1
+        elif char.islower(): inLower += 1
+        elif char.isdigit(): inDigit += 1
+    print("In Hoa=",inSupper,"\nIn Thường =",inLower,"\nSố =",inDigit,"\nTổng =",len(strings))
+# bai46()
+
+# Bài 47: Nhập vào một chuỗi, hãy đếm xem trong chuỗi có bao nhiêu ký tự in hoa, bao nhiêu ký tự in thường, bao nhiêu ký tự số
 # def bai47():
-# def bai48():
-# def bai49():
-# def bai50():
-# def bai51():
-# def bai52():
+# bai47()
+
+# Bài 48: Nhập vào một chuỗi, hãy tách toàn bộ ký tự số trong chuỗi ra rồi tính tổng của chúng
+# VD:Nhập chuỗi: abd45ecf47wde3s1 - Tổng: 4 + 5 + 4 + 7 + 3 + 1 = 24
+def bai48():
+    strings = str(input("Nhập chuỗi:"))
+    inDigit = 0
+    sum = 0
+    for char in strings:
+        if char.isdigit():
+            sum += int(char)
+            inDigit += 1
+    print("Tổng các ký tự số là:", sum)
+# bai48()
+
+# Bài 49: Nhập vào một chuỗi, hãy tách toàn bộ con số trong chuỗi ra rồi tính tổng của chúng
+# VD:Nhập chuỗi: abd45ecf47wde3s1 - Tổng: 45 + 47 + 3 + 1 = 96
+def bai49():
+    import re
+    strings = str(input("Nhập chuỗi:"))
+    numbers = re.findall(r'\d+', strings)
+    total = sum(map(int, numbers))
+    print("Tổng các con số là:", total)
+# bai49()
+
+# Bài 50: Nhập vào một chuỗi, kiểm tra chuỗi đó có phải là một chuỗi mật khẩu mạnh hay không
+# (một chuỗi mật khẩu mạnh cần có ít nhất 1 ký tự đặc biệt, 1 ký tự in hoa, 1 con số, 1 chữ thường và độ dài phải lớn hơn 6)
+
+def bai50():
+    import re
+    password = str(input("Nhập mật khẩu: "))
+    noti = "Mật khẩu yếu, yêu cầu phải có ít nhất:\n1 ký tự đặc biệt.\n1 ký tự in hoa.\n1 con số.\n1 chữ thường.\nđộ dài phải lớn hơn 6."
+    if len(password) <= 6:print(noti)
+    elif not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):print(noti)
+    elif not re.search(r'[A-Z]', password):print(noti)
+    elif not re.search(r'\d', password):print(noti)
+    elif not re.search(r'[a-z]', password):print(noti)
+    else:print("Mật khẩu mạnh")
+# bai50()
+
+
+# Bài 51: Nhập vào một số nguyên, hãy chuyển số sang chuỗi, rồi đặt dấu chấm phân tách mỗi 3 chữ số 
+# (phân cách phần ngàn) rồi in ra màn hình
+# VD: Nhập số: 375469485 - Đổi sang chuỗi rồi in ra: 375.469.485
+def bai51():
+    number = int(input("Nhập số nguyên: "))
+    number_str = str(int(number))
+    
+    formatted_str = ""
+    count = 0
+
+    for digit in reversed(number_str):
+        if count == 3:
+            formatted_str = "." + formatted_str
+            count = 0
+        formatted_str = digit + formatted_str
+        count += 1
+
+    print("Đã chuyển", formatted_str)
+# bai51()
+
+# Bài 52: Nhập vào chuỗi a và chuỗi b
+# Hãy xóa chuỗi b trong chuỗi a rồi in lại chuỗi a ra màn hình (không dùng hàm replace)
+# Ví dụ:
+# Chuỗi a: "Xin chào mọi người!"
+# Chuỗi b: "Xin chào"
+# Sau khi xóa, chuỗi a: " mọi người!"
+def bai52():
+    a = input("Nhập chuỗi a: ")
+    b = input("Nhập chuỗi b: ")
+    index = a.find(b)
+    if index == -1:
+        print("Chuỗi b không tồn tại trong chuỗi a.")
+    else:
+        part1 = a[:index]
+        part2 = a[index + len(b):]
+        print("Sau khi xóa, chuỗi a:", part1 + part2)
+bai52()
