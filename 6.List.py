83c79331a683571fc7ff479e6d23316030234ad6
HHO TruongNL <truongnl@hunghau.vn>
2023-09-04 17:27:06 +0700

    update

--- 6.List.py
+++ 6.List.py
@@ -1,20 +1,79 @@
+
 # def bai53():
+# bai53()
+
+
 # def bai54():
+# bai54()
+
+
 # def bai55():
+# bai55()
+
+
 # def bai56():
+# bai56()
+
+
 # def bai57():
+# bai57()
+
+
 # def bai58():
+# bai58()
+
+
 # def bai59():
+# bai59()
+
+
 # def bai60():
+# bai60()
+
+
 # def bai61():
+# bai61()
+
+
 # def bai62():
+# bai62()
+
+
 # def bai63():
+# bai63()
+
+
 # def bai64():
+# bai64()
+
+
 # def bai65():
+# bai65()
+
+
 # def bai66():
+# bai66()
+
+
 # def bai67():
+# bai67()
+
+
 # def bai68():
+# bai68()
+
+
 # def bai69():
+# bai69()
+
+
 # def bai70():
+# bai70()
+
+
 # def bai71():
+# bai71()
+
+
 # def bai72():
+# bai72()
