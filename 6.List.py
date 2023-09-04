32fad899ac81a4001c048283b8fffc04225539a8
HHO TruongNL <truongnl@hunghau.vn>
2023-09-04 17:31:47 +0700

    update

--- 6.List.py
+++ 6.List.py
@@ -1,79 +1,60 @@
-
+# Bài 53
 # def bai53():
 # bai53()
-
-
+# Bài 54
 # def bai54():
 # bai54()
-
-
+# Bài 55
 # def bai55():
 # bai55()
-
-
+# Bài 56
 # def bai56():
 # bai56()
-
-
+# Bài 57
 # def bai57():
 # bai57()
-
-
+# Bài 58
 # def bai58():
 # bai58()
-
-
+# Bài 59
 # def bai59():
 # bai59()
-
-
+# Bài 60
 # def bai60():
 # bai60()
-
-
+# Bài 61
 # def bai61():
 # bai61()
-
-
+# Bài 62
 # def bai62():
 # bai62()
-
-
+# Bài 63
 # def bai63():
 # bai63()
-
-
+# Bài 64
 # def bai64():
 # bai64()
-
-
+# Bài 65
 # def bai65():
 # bai65()
-
-
+# Bài 66
 # def bai66():
 # bai66()
-
-
+# Bài 67
 # def bai67():
 # bai67()
-
-
+# Bài 68
 # def bai68():
 # bai68()
-
-
+# Bài 69
 # def bai69():
 # bai69()
-
-
+# Bài 70
 # def bai70():
 # bai70()
-
-
+# Bài 71
 # def bai71():
 # bai71()
-
-
+# Bài 72
 # def bai72():
 # bai72()
